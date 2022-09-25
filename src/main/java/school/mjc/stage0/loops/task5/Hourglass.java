226b620ca1a66137f570003d60f1896228f25cef
Batuhan Derya <bderya@3-G-9.42heilbronn.de>
2022-09-25 23:46:30 +0200

    DEEEE

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -1,7 +1,22 @@
 package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
-    public void printHourglassOfGivenSize(int height) {
-
+    public void printHourglassOfGivenSize(int height)
+	{
+		for (int i = 0; i < height; i++)
+		{
+            for (int j = 0; j < height; j++)
+			{
+                if (j >= i && j < height - i || j >= height - i - 1 && j <= i)
+				{
+                    System.out.print(8);
+                }
+				else
+				{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -1,7 +1,27 @@
 package school.mjc.stage0.loops.task5;
 
-public class Triangle {
-    public void printTriangle(int cathetusLength) {
-
+public class Triangle
+{
+    public void printTriangle(int cathetusLength)
+	{
+		for (int i = 0; i < cathetusLength; i++)
+		{
+            for (int j = 0; j < cathetusLength; j++)
+			{
+                if (i == cathetusLength - 1)
+				{
+                    System.out.print(8);
+                }
+				else if (j <= i)
+				{
+                    System.out.print(8);
+                }
+				else
+				{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
